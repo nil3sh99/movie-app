09855eb8449046194c4c135926a3b8de4cdbb6ba
Nilesh Aggarwal <agnilesh8199@gmail.com>
2021-05-22 17:14:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 	const [searchValue, setSearchValue] = useState('');
 
 	const getMovieRequest = async (searchValue) => {
-		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=c7a8f9b9`;
+		const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=c7a8f9b9`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
